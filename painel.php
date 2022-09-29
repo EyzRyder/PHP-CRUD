41929d78c1e7bbe37f7a96f48be9fce90ad56bd9
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-09-29 20:09:51 -0300

    .

--- painel.php
+++ painel.php
@@ -8,6 +8,19 @@ include('conexao.php');
 mysqli_select_db($conexao, "nossobanco");
 $query = sprintf("SELECT id, first_name, last_name, address FROM user");
 $dados = mysqli_query($conexao, $query) or die(mysqli_connect_error() . PHP_EOL);
+
+if (array_key_exists('logout', $_POST)) {
+    logout();
+}
+
+function logout()
+{
+    session_destroy();
+    header('Location: index.html');
+    exit();
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="br">
@@ -22,18 +35,18 @@ $dados = mysqli_query($conexao, $query) or die(mysqli_connect_error() . PHP_EOL)
 </head>
 
 <body>
-    <div id="box">
-    <h4>User: <?php echo $_SESSION['name']; ?></h4>
-    <button><a href="logout.php">Sair</a></button>
-    </div>
+    <form method="post" id="box">
+        <h4>User: <?php echo $_SESSION['name']; ?></h4>
+        <button name="logout">Sair</button>
+    </form>
     <center>
-        
+
         <form action="cadastro.php" method="POST" class="cadastro">
             <h2>Cadastrar</h2>
             <input type="text" name="nome" id="nome" placeholder="Digite seu nome" required />
             <input type="text" name="sobrenome" id="sobrenome" placeholder="Digite seu sobre nome" required />
             <input type="text" name="endereco" id="endereco" placeholder="Digite seu endereço" required />
-            <button type="submit" id="submit">input</button>
+            <button name="btn" type="submit" id="submit">input</button>
         </form>
         <h1>Dados inseridos</h1>
         <table border=1>
@@ -43,17 +56,20 @@ $dados = mysqli_query($conexao, $query) or die(mysqli_connect_error() . PHP_EOL)
                     <th>Nome</th>
                     <th>Sobrenome</th>
                     <th>Endereço</th>
+                    <th>ação</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-
                 while ($row = $dados->fetch_assoc()) {
                     echo '<tr>';
                     echo '<td>' . $row['id'] . '</td>';
                     echo '<td>' . $row['first_name'] . '</td>';
                     echo '<td>' . $row['last_name'] . '</td>';
                     echo '<td>' . $row['address'] . '</td>';
+                    echo '<td> <form method="POST"> 
+                    <button name="editRow">Edit</button> ';
+                    echo ' <button name="deleteRow">Delete</button> </form> </td>';
                     echo '</tr>';
                 }
                 ?>
