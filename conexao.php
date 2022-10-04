c4b15581b38b6c3441bc789e7d79b22db6dfb68e
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-10-04 13:45:06 -0300

    added text

--- conexao.php
+++ conexao.php
@@ -4,11 +4,12 @@ define('USUARIO', 'root');
 define('SENHA', 'raizV2toorU2');
 define('DB', 'nossobanco');
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB);
-        if (!$conexao) {
-            echo 'Erro: Unable to connect to MySQL.' . PHP_EOL;
-            echo 'Debugging erro: ' . mysqli_connect_error() . PHP_EOL;
-            echo 'Debugging erro: ' . mysqli_connect_error() . PHP_EOL;
-            exit;
-        }
-?>
+    $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB);
+
+    if (!$conexao) {
+        echo"não conectou ao banco de dados";
+        echo 'Erro: Unable to connect to MySQL.' . PHP_EOL;
+        echo 'Debugging erro: ' . mysqli_connect_error() . PHP_EOL;
+        echo 'Debugging erro: ' . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
